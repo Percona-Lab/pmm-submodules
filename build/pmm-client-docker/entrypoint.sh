f52771277c8a379cfc29caea77f5f038b7725f6a
Mykola Marzhan <mykola.marzhan@percona.com>
2018-09-18 12:54:07 +0300

    PMM-2891 add waiting functions

--- build/pmm-client-docker/entrypoint.sh
+++ build/pmm-client-docker/entrypoint.sh
@@ -3,6 +3,37 @@
 set -o errexit
 set -o xtrace
 
+wait_for_url() {
+    local URL=$1
+
+    for i in `seq 6` ; do
+        curl -k "${URL}" > /dev/null 2>&1 && result=$? || result=$?
+        if [ $result -eq 0 ] ; then
+            return
+        fi
+        sleep 10
+    done
+
+    echo "Operation timed out" >&2
+    exit 1
+}
+
+wait_for_port() {
+    local HOST=$1
+    local PORT=$2
+
+    for i in `seq 6` ; do
+        nc -z "${HOST}" "${PORT}" > /dev/null 2>&1 && result=$? || result=$?
+        if [ $result -eq 0 ] ; then
+            return
+        fi
+        sleep 10
+    done
+
+    echo "Operation timed out" >&2
+    exit 1
+}
+
 if [ -z "${PMM_SERVER}" ]; then
     echo PMM_SERVER is not specified. exiting
     exit 1
@@ -17,6 +48,9 @@ fi
 PMM_SERVER_IP=$(ping -c 1 "${PMM_SERVER}" | grep PING | sed -e 's/).*//; s/.*(//')
 SRC_ADDR=$(ip route get "${PMM_SERVER_IP}" | grep 'src ' | awk '{print$7}')
 CLIENT_NAME=${DB_HOST:-$HOSTNAME}
+
+wait_for_url "https://${PMM_USER}:${PMM_PASSWORD}@${PMM_SERVER}/qan-api/ping"
+wait_for_url "https://${PMM_USER}:${PMM_PASSWORD}@${PMM_SERVER}/v1/agent/checks"
 
 pmm-admin config \
     --skip-root \
@@ -36,6 +70,13 @@ if [ -n "${DB_USER}" ]; then
 fi
 if [ -n "${DB_PASSWORD}" ]; then
     DB_ARGS+=" --password ${DB_PASSWORD}"
+fi
+if [ -n "${DB_PORT}" ]; then
+    DB_ARGS+=" --port ${DB_PORT}"
+fi
+
+if [ -n "${DB_HOST}" -a "${DB_PORT}" ]; then
+    wait_for_port "${DB_HOST}" "${DB_PORT}"
 fi
 
 if [ -n "${DB_TYPE}" ]; then
