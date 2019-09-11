cdb576a48776bdd04b7906f1afc5e93c9d1335af
Ivan Pylypenko <ivan.pylypenko@percona.com>
2019-09-11 11:47:59 +0300

    CLOUD-374 Use updated server status url

--- build/pmm-client-docker/entrypoint.sh
+++ build/pmm-client-docker/entrypoint.sh
@@ -35,54 +35,145 @@ wait_for_port() {
     exit 1
 }
 
-if [ -z "${PMM_SERVER}" ]; then
-    echo PMM_SERVER is not specified. exiting
-    exit 1
-fi
-if [ -n "${PMM_USER}" ]; then
-    ARGS+=" --server-user ${PMM_USER}"
-fi
-if [ -n "${PMM_PASSWORD}" ]; then
-    ARGS+=" --server-password ${PMM_PASSWORD}"
-fi
+pmm2_start() {
+    AGENT_CONFIG_FILE=/usr/local/percona/pmm2/config/pmm-agent.yaml
+    rm -f ${AGENT_CONFIG_FILE}
+    if [ -n "${PMM_USER}" ]; then
+        ARGS+=" --server-username=${PMM_USER}"
+    fi
 
-PMM_SERVER_IP=$(ping -c 1 "${PMM_SERVER/:*/}" | grep PING | sed -e 's/).*//; s/.*(//')
-SRC_ADDR=$(ip route get "${PMM_SERVER_IP}" | grep 'src ' | sed -e 's/.* src //; s/ .*//')
-CLIENT_NAME="${HOSTNAME}"
+    pmm-agent setup \
+        --force \
+        --config-file=${AGENT_CONFIG_FILE} \
+        --server-address=${PMM_SERVER} \
+        --server-insecure-tls \
+        --container-id=${HOSTNAME} \
+        --container-name=${CLIENT_NAME} \
+        --ports-min=${CLIENT_PORT_MIN:-30100} \
+        --ports-max=${CLIENT_PORT_MAX:-30200} \
+        --listen-port=${CLIENT_PORT_LISTEN:-7777} \
+        ${ARGS} \
+        "${CLIENT_ADDR:-$SRC_ADDR}" container "${CLIENT_NAME}"
 
-wait_for_url "https://${PMM_USER}:${PMM_PASSWORD}@${PMM_SERVER}/v1/status/leader" "127.0.0.1:8300"
+    if [ -n "${DB_USER}" ]; then
+        DB_ARGS+=" --username=${DB_USER}"
+    fi
+    if [ -n "${DB_PASSWORD}" ]; then
+        DB_ARGS+=" --password=${DB_PASSWORD}"
+    fi
+    if [ -n "${DB_CLUSTER}" ]; then
+        DB_ARGS+=" --cluster=${DB_CLUSTER}"
+    fi
 
-pmm-admin config \
-    --skip-root \
-    --force \
-    --server "${PMM_SERVER}" \
-    --server-insecure-ssl \
-    --bind-address "${SRC_ADDR}" \
-    --client-address "${SRC_ADDR}" \
-    --client-name "${CLIENT_NAME}" \
-    ${ARGS}
+    if [ -n "${DB_HOST}" -a "${DB_PORT}" ]; then
+        wait_for_port "${DB_HOST}" "${DB_PORT}"
+    fi
 
-if [ -n "${DB_HOST}" ]; then
-    DB_ARGS+=" --host ${DB_HOST}"
-fi
-if [ -n "${DB_USER}" ]; then
-    DB_ARGS+=" --user ${DB_USER}"
-fi
-if [ -n "${DB_PASSWORD}" ]; then
-    DB_ARGS+=" --password ${DB_PASSWORD}"
-fi
-if [ -n "${DB_PORT}" ]; then
-    DB_ARGS+=" --port ${DB_PORT}"
-fi
+    cat "${AGENT_CONFIG_FILE}"
 
-if [ -n "${DB_HOST}" -a "${DB_PORT}" ]; then
-    wait_for_port "${DB_HOST}" "${DB_PORT}"
-fi
+    pmm-agent --config-file="${AGENT_CONFIG_FILE}" \
+        --ports-min="${CLIENT_PORT_MIN:-30100}" \
+        --ports-max="${CLIENT_PORT_MAX:-30200}" > /usr/local/percona/pmm2/pmm-agent-tmp.log 2>&1 &
 
-if [ -n "${DB_TYPE}" ]; then
-    pmm-admin add "${DB_TYPE}" \
+    wait_for_url "http://127.0.0.1:7777"
+
+    cat /usr/local/percona/pmm2/pmm-agent-tmp.log
+
+    if [ -n "${DB_TYPE}" ]; then
+        case "${DB_TYPE}" in
+            mongodb )
+                DB_ARGS=$(echo ${DB_ARGS} | sed -r 's/^(--uri=\w+:(\/?\/?)[^\s]+\s)(--.*)|^(--uri.*$)/\3/g')
+                ;;
+        esac
+        pmm-admin add "${DB_TYPE}" \
+            --skip-connection-check \
+            --server-url="https://${PMM_USER}:${PMM_PASSWORD}@${PMM_SERVER}/" \
+            --server-insecure-tls \
+            ${DB_ARGS} \
+            "${DB_HOST}:${DB_PORT}"
+    fi
+
+    kill %1
+    exec pmm-agent \
+        --config-file=/usr/local/percona/pmm2/config/pmm-agent.yaml \
+        --ports-min="${CLIENT_PORT_MIN:-30100}" \
+        --ports-max="${CLIENT_PORT_MAX:-30200}"
+}
+
+pmm_start() {
+    cd /usr/local/percona/pmm-client
+    if [ -n "${PMM_USER}" ]; then
+        ARGS+=" --server-user=${PMM_USER}"
+    fi
+    
+    pmm-admin config \
         --skip-root \
-        ${DB_ARGS}
-fi
+        --force \
+        --server "${PMM_SERVER}" \
+        --server-insecure-ssl \
+        --bind-address "${SRC_ADDR}" \
+        --client-address "${SRC_ADDR}" \
+        --client-name "${CLIENT_NAME}" \
+        ${ARGS}
 
-exec "$@"
+    if [ -n "${DB_HOST}" ]; then
+        DB_ARGS+=" --host ${DB_HOST}"
+    fi
+    if [ -n "${DB_USER}" ]; then
+        DB_ARGS+=" --user ${DB_USER}"
+    fi
+    if [ -n "${DB_PASSWORD}" ]; then
+        DB_ARGS+=" --password ${DB_PASSWORD}"
+    fi
+    if [ -n "${DB_PORT}" ]; then
+        DB_ARGS+=" --port ${DB_PORT}"
+    fi
+
+    if [ -n "${DB_HOST}" -a "${DB_PORT}" ]; then
+        wait_for_port "${DB_HOST}" "${DB_PORT}"
+    fi
+
+    if [ -n "${DB_TYPE}" ]; then
+        case "${DB_TYPE}" in
+            proxysql )
+                pmm-admin add "${DB_TYPE}" \
+                    --skip-root \
+                    --dsn "${DB_USER}:${DB_PASSWORD}@tcp(${DB_HOST}:${DB_PORT})/" 
+                ;;
+            * )
+                pmm-admin add "${DB_TYPE}" \
+                    --skip-root \
+                    ${DB_ARGS}
+                ;;
+        esac
+
+    fi
+    exec pid-watchdog
+}
+
+main() {
+    if [ -z "${PMM_SERVER}" ]; then
+        echo PMM_SERVER is not specified. exiting
+        exit 1
+    fi
+    if [ -n "${PMM_PASSWORD}" ]; then
+        ARGS+="--server-password=${PMM_PASSWORD}"
+    fi
+
+    PMM_SERVER_IP=$(ping -c 1 "${PMM_SERVER/:*/}" | grep PING | sed -e 's/).*//; s/.*(//')
+    SRC_ADDR=$(ip route get "${PMM_SERVER_IP}" | grep 'src ' | sed -e 's/.* src //; s/ .*//')
+    CLIENT_NAME="${CLIENT_NAME:-$HOSTNAME}"
+
+    SERVER_RESPONSE_CODE=$(curl -k -s -o /dev/null -w "%{http_code}" "https://${PMM_USER}:${PMM_PASSWORD}@${PMM_SERVER}/v1/readyz")
+
+    if [[ "${SERVER_RESPONSE_CODE}" == '200' ]]; then
+        export PATH="/usr/local/percona/pmm2/bin/:${PATH}"
+        pmm2_start
+    else
+        export PATH="/usr/local/percona/pmm-client/:/usr/local/percona/qan-agent/bin/:${PATH}"
+        pmm_start
+    fi
+
+}
+
+main
