ccaab526e41657af95784b7ef29b2a30347e8a32
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-02-10 13:22:55 +0100

    PMM-4796 Fix linter warnings.

--- build/pmm-client-docker/entrypoint.py
+++ build/pmm-client-docker/entrypoint.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python
 
-from __future__ import print_function, unicode_literals
-import os
-import subprocess
-import sys
-from distutils.util import strtobool
-
-
-help = """
+"""
 PMM 2.x Client Docker container.
 
-It runs pmm-agent as a process with PID 1. It is configured entirely by environment variables. Arguments or flags are not used.
+It runs pmm-agent as a process with PID 1.
+It is configured entirely by environment variables. Arguments or flags are not used.
 
 The following environment variables are recognized by the Docker entrypoint:
 * PMM_AGENT_SETUP - if true, `pmm-agent setup` is called before `pmm-agent run`.
 
-Additionally, the many environment variables are recognized by pmm-agent itself. The following help text shows them as [PMM_AGENT_XXX].
+Additionally, the many environment variables are recognized by pmm-agent itself.
+The following help text shows them as [PMM_AGENT_XXX].
 """
+
+from __future__ import print_function, unicode_literals
+import os
+import sys
+from distutils.util import strtobool
+
 
 PMM_AGENT_SETUP = strtobool(os.environ.get('PMM_AGENT_SETUP', 'false'))
 
 
 def main():
+    """
+    Entrypoint.
+    """
+
     if len(sys.argv) > 1:
-        print(help)
+        print(__doc__)
         os.system('pmm-agent setup --help')
         sys.exit(1)
 
