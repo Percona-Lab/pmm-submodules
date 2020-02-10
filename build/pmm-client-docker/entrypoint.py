db8c572e9853f6cceb61a30e6e7875b101d61343
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2020-02-10 11:49:04 +0100

    PMM-4796 Expand help text.

--- build/pmm-client-docker/entrypoint.py
+++ build/pmm-client-docker/entrypoint.py
@@ -1,40 +1,44 @@
 #!/usr/bin/env python
-'''
-Basic Kubernetes-compatible PMM Client Docker entrypoint.
-
-To configure pmm-agent set environment variable PMM_AGENT_SETUP=true.
-This will run:
-`pmm-agent setup --config-file=/usr/local/percona/pmm2/config/pmm-agent.yaml`
-'''
 
 from __future__ import print_function, unicode_literals
-from distutils.util import strtobool
-from subprocess import Popen
-
 import os
+import subprocess
 import sys
+from distutils.util import strtobool
 
 
-# if PMM_AGENT_SETUP is True run:
-# `pmm-agent setup --config-file=/usr/local/percona/pmm2/config/pmm-agent.yaml`
-PMM_AGENT_SETUP = False
+help = """
+PMM 2.x Client Docker container.
+
+It runs pmm-agent as a process with PID 1. It is configured entirely by environment variables. Arguments or flags are not used.
+
+The following environment variables are recognized by the Docker entrypoint:
+* PMM_AGENT_SETUP - if true, `pmm-agent setup` is called before `pmm-agent run`.
+
+Additionally, the many environment variables are recognized by pmm-agent itself. The following help text shows them as [PMM_AGENT_XXX].
+"""
+
+
+PMM_AGENT_SETUP = strtobool(os.environ.get('PMM_AGENT_SETUP', 'false'))
 PMM_AGENT_CONFIG = '/usr/local/percona/pmm2/config/pmm-agent.yaml'
 
 
 def main():
-    '''Entrypoint.'''
-    # Setup pmm-agent.
-    if PMM_AGENT_SETUP or strtobool(os.environ.get('PMM_AGENT_SETUP', 'FALSE')):
-        print('setup pmm-agent')
-        cmd = ('pmm-agent', 'setup', '--config-file=%s' % (PMM_AGENT_CONFIG,))
-        returncode = Popen(cmd, universal_newlines=True).wait()
-        if returncode != os.EX_OK:
-            sys.exit(returncode)
+    if len(sys.argv) > 1:
+        print(help)
+        os.system('pmm-agent setup --help')
+        sys.exit(1)
 
-    print('Starting pmm-agent: `pmm-agent '
-          '--config-file=%s' % (PMM_AGENT_CONFIG,))
-    # Run pmm-agent.
-    os.execlp('pmm-agent', '--config-file=%s' % (PMM_AGENT_CONFIG,))
+    if PMM_AGENT_SETUP:
+        print('Starting pmm-agent setup ...')
+        status = os.system('pmm-agent setup --config-file=' + PMM_AGENT_CONFIG)
+        if status != os.EX_OK:
+            sys.exit(status)
+
+    print('Starting pmm-agent ...')
+    sys.stdout.flush()
+    sys.stderr.flush()
+    os.execlp('pmm-agent', 'run', '--config-file=' + PMM_AGENT_CONFIG)
 
 
 if __name__ == '__main__':
