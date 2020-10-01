f0adacdd54caa4d477a9a394036587dce5eba1d2
Alexey Palazhchenko <alexey.palazhchenko@percona.com>
2020-10-01 12:24:26 +0300

    PMM-6633 Add PMM_AGENT_ENTRYPOINT_DELAY. (#1141)
    It is needed now for PoCs and SaaS monitoring.

--- build/pmm-client-docker/entrypoint.py
+++ build/pmm-client-docker/entrypoint.py
@@ -7,7 +7,8 @@ It runs pmm-agent as a process with PID 1.
 It is configured entirely by environment variables. Arguments or flags are not used.
 
 The following environment variables are recognized by the Docker entrypoint:
-* PMM_AGENT_SETUP - if true, `pmm-agent setup` is called before `pmm-agent run`.
+* PMM_AGENT_ENTRYPOINT_DELAY - if non-zero, entrypoint execution is delayed for a given number of seconds.
+* PMM_AGENT_SETUP            - if true, `pmm-agent setup` is called before `pmm-agent run`.
 
 Additionally, the many environment variables are recognized by pmm-agent itself.
 The following help text shows them as [PMM_AGENT_XXX].
@@ -16,10 +17,12 @@ The following help text shows them as [PMM_AGENT_XXX].
 from __future__ import print_function, unicode_literals
 import os
 import sys
+import time
 from distutils.util import strtobool
 
 
-PMM_AGENT_SETUP = strtobool(os.environ.get('PMM_AGENT_SETUP', 'false'))
+PMM_AGENT_ENTRYPOINT_DELAY = int(os.environ.get('PMM_AGENT_ENTRYPOINT_DELAY', '0'))
+PMM_AGENT_SETUP            = strtobool(os.environ.get('PMM_AGENT_SETUP', 'false'))
 
 
 def main():
@@ -31,6 +34,10 @@ def main():
         print(__doc__, file=sys.stderr)
         os.system('pmm-agent setup --help')
         sys.exit(1)
+
+    if PMM_AGENT_ENTRYPOINT_DELAY:
+        print('Sleeping {} second(s) ...'.format(PMM_AGENT_ENTRYPOINT_DELAY), file=sys.stderr)
+        time.sleep(PMM_AGENT_ENTRYPOINT_DELAY)
 
     if PMM_AGENT_SETUP:
         print('Starting pmm-agent setup ...', file=sys.stderr)
